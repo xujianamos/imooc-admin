84628c3a73506ec5131084c2d83007c8503f67df
徐建 <xujian@cnksi.com>
2021-12-09 14:38:00 +0800

    feat(路由): 新增公开路由表

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,30 @@
+/*
+ * @Author: xujian
+ * @Date: 2021-12-07 10:57:32
+ * @LastEditors: xujian
+ * @LastEditTime: 2021-12-09 10:39:19
+ * @Description:路由配置
+ * @FilePath: \imooc-admin\src\router\index.js
+ */
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-const routes = []
+/**
+ * 公开路由表
+ */
+const publicRoutes = [
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: () => import('@/views/login')
+  }
+]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes: publicRoutes
 })
 
 export default router
