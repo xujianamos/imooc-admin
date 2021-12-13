260bf213702d588ecc4a66c218504dd101fa0c1c
徐建 <xujian@cnksi.com>
2021-12-13 18:03:32 +0800

    feat(store): 修改store入口文件

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,18 @@
+/*
+ * @Author: xujian
+ * @Date: 2021-12-07 10:57:32
+ * @LastEditors: xujian
+ * @LastEditTime: 2021-12-13 17:56:26
+ * @Description:store 入口文件
+ * @FilePath: \imooc-admin\src\store\index.js
+ */
 import { createStore } from 'vuex'
-
+import user from './modules/user.js'
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state: {},
+  mutations: {},
+  actions: {},
   modules: {
+    user
   }
 })
