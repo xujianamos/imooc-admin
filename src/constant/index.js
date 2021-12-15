46e005d21ab27524096d871bcea43c9cbc67e911
徐建 <xujian@cnksi.com>
2021-12-15 09:10:35 +0800

    feat(缓存处理): 本地缓存处理方案

---
+++ src/constant/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: xujian
+ * @Date: 2021-12-15 09:09:06
+ * @LastEditors: xujian
+ * @LastEditTime: 2021-12-15 09:09:07
+ * @Description:常量配置
+ * @FilePath: \imooc-admin\src\constant\index.js
+ */
+export const TOKEN = 'token'
