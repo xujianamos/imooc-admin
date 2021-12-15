a2991b3f37116539f26c1bb75312b4400b020574
徐建 <xujian@cnksi.com>
2021-12-15 09:12:00 +0800

    feat(token): 保存服务端返回的token

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -2,16 +2,25 @@
  * @Author: xujian
  * @Date: 2021-12-13 17:55:39
  * @LastEditors: xujian
- * @LastEditTime: 2021-12-13 17:55:40
+ * @LastEditTime: 2021-12-15 09:11:21
  * @Description:用于处理所有和 用户相关 的内容
  * @FilePath: \imooc-admin\src\store\modules\user.js
  */
 import { login } from '@/api/sys'
 import md5 from 'md5'
+import { setItem, getItem } from '@/utils/storage'
+import { TOKEN } from '@/constant'
 export default {
   namespaced: true,
-  state: () => ({}),
-  mutations: {},
+  state: () => ({
+    token: getItem(TOKEN) || ''
+  }),
+  mutations: {
+    setToken(state, token) {
+      state.token = token
+      setItem(TOKEN, token)
+    }
+  },
   actions: {
     login(context, userInfo) {
       const { username, password } = userInfo
@@ -21,6 +30,7 @@ export default {
           password: md5(password)
         })
           .then(data => {
+            this.commit('user/setToken', data.data.data.token)
             resolve()
           })
           .catch(err => {
