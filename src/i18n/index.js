dd952a1010711427f7936300c3cc3f5a28bad181
xujian <2513573453@qq.com>
2021-12-20 17:21:38 +0800

    refactpr(代码优化): 代码优化

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,13 +2,13 @@
  * @Author: xujian
  * @Date: 2021-12-17 23:13:56
  * @LastEditors: xujian
- * @LastEditTime: 2021-12-17 23:31:23
- * @Description:国际化
- * @FilePath: /imooc-admin/src/i18n/index.js
+ * @LastEditTime: 2021-12-20 11:20:42
+ * @Description:国际化i18n配置
+ * @FilePath: \imooc-admin\src\i18n\index.js
  */
 import { createI18n } from 'vue-i18n'
 
-// 1.创建 `messages` 数据源
+// 1.创建 messages 数据源
 const messages = {
   en: {
     msg: {
@@ -22,10 +22,10 @@ const messages = {
   }
 }
 
-// 2.创建 `locale` 语言变量
+// 2.创建 locale语言变量
 const locale = 'en'
 
-// 3.初始化 `i18n` 实例
+// 3.初始化 i18n 实例
 
 const i18n = createI18n({
   // 使用 Composition API 模式，则需要将其设置为false
@@ -36,5 +36,5 @@ const i18n = createI18n({
   messages
 })
 
-// 4.把 `i18n` 注册到 `vue` 实例
+// 4.把 i18n 注册到 vue 实例
 export default i18n
