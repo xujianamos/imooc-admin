acfae0976be46131224fe15a0ca70d1c77862b21
“xujian“ <2513573453@qq.com>
2021-12-22 22:11:31 +0800

    refactpr(优化全屏组件): 优化全屏组件

--- src/components/Screenfull/index.vue
+++ src/components/Screenfull/index.vue
@@ -2,13 +2,13 @@
  * @Author: xujian
  * @Date: 2021-12-22 16:41:19
  * @LastEditors: xujian
- * @LastEditTime: 2021-12-22 16:41:20
+ * @LastEditTime: 2021-12-22 22:07:29
  * @Description: 全屏组件
- * @FilePath: \imooc-admin\src\components\Screenfull\index.vue
+ * @FilePath: /imooc-admin/src/components/Screenfull/index.vue
 -->
 <template>
-  <div>
-    <svg-icon :icon="isFullscreen ? 'exit-fullscreen' : 'fullscreen'" @click="onToggle" />
+  <div @click="onToggle">
+    <svg-icon :icon="isFullscreen ? 'exit-fullscreen' : 'fullscreen'" />
   </div>
 </template>
 
