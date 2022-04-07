1caa39688f97fdc534401598aa91f98fcb1e2868
xujian <2513573453@qq.com>
2022-04-07 23:28:53 +0800

    feat(配置):修改系统图标以及增加登陆验证

--- src/utils/request.js
+++ src/utils/request.js
@@ -20,6 +20,8 @@ const service = axios.create({
 // 请求拦截器
 service.interceptors.request.use(
   config => {
+    // 添加 icode
+    config.headers.icode = 'BABD66E06099E542'
     // 在这个位置需要统一的去注入token
     if (store.getters.token) {
       // 判断token是否超时
